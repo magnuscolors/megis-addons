3dcc24d88298861a8885cf9e75e3cee6c7d9d7ae
Willem Hulshof <w.hulshof@magnus.nl>
2015-01-04 22:52:11 +0100

    prodversie megis addons jan2014

---
+++ megis_account_slam/account_slam.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+from openerp.osv import fields, osv
+from openerp import tools
+from openerp.tools.translate import _
+
+class account_analytic_line(osv.osv):
+    _inherit = 'account.analytic.line'
+    _description = 'Analytic Line'
+    _columns = {
+        'regel_naar_slam': fields.boolean('Regel is naar SLAM'),
+        'datum_naar_slam': fields.date('Datum regel naar SLAM', 'required=False'),
+    }
+
+account_analytic_line()
+
+class account_journal(osv.osv):
+    _inherit = 'account.journal'
+    _description = 'Journal'
+    _columns = {
+        'slam_relevant': fields.boolean('Slam Export', help='If the journal entries of theis journal should be exported to SLAM'),
+    }
+
+    _defaults = {
+        'slam_relevant': False,
+    }
+
+account_journal()
